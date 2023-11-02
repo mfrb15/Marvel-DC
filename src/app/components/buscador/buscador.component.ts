5233d7b33cb47c7abee067853661a46e1ee5dd9b
mfrb15 <mrodriguezbello26@gmail.com>
2023-11-02 19:38:17 -0300

    editando busqueda

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,21 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HeroesService } from 'src/app/services/heroes.service';
 import { ActivatedRoute } from '@angular/router';
 import { HeroesComponent } from '../heroes/heroes.component';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
 
 })
 export class BuscadorComponent implements OnInit {
-heroes: any[]= [];
+@Input() heroes: any= {};
+@Input() index?: number;
 termino: string = '';
 
 
 
 
   constructor(private activatedRoute: ActivatedRoute,
-    private _heroesService: HeroesService) { }
+    private _heroesService: HeroesService, private router: Router) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
@@ -23,6 +25,11 @@ termino: string = '';
       this.heroes = this._heroesService.buscarHeroes(params['termino']);
       console.log(this.heroes);
     })
+  }
+
+  verHeroe(){
+    console.log(this.index);
+    this.router.navigate(['/heroe', this.index])
   }
 
 
--- src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,14 +10,19 @@ export class HeroeTarjetaComponent implements OnInit {
   @Input() heroe: any = {};
   @Input() index?: number;
 
-  constructor(private router: Router) { }
+  @Output() heroeSeleccionado: EventEmitter<number>;
+
+
+  constructor(private router: Router) {
+    this.heroeSeleccionado = new EventEmitter();
+   }
 
   ngOnInit(): void {
   }
 
   verHeroe() {
     this.router.navigate(['/heroe', this.index])
-    console.log(this.index)
+  //  this.heroeSeleccionado.emit(this.index);
   }
 
 }
